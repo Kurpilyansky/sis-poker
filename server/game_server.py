7982e4e4bdc7e19ba1ccc209bcc1371937e2f4fa
Eugene Kurpilyansky <dembelz@yandex.ru>
2017-07-14 16:30:00 +0300

    chips

--- server/game_server.py
+++ server/game_server.py
@@ -11,24 +11,31 @@ FLOP = 'FLOP'
 TURN = 'TURN'
 RIVER = 'RIVER'
 
+FOLD = 'FOLD'
+CALL = 'CALL'
+RAISE = 'RAISE'
+ALL_IN = 'ALL_IN'
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 class Player:
-  def __init__(self, name, pos):
+  def __init__(self, name, pos, chips):
     self.name = name
     self.pos = pos
+    self.chips = chips
     self.hand = []
 
   def as_dict(self):
     return {'name': self.name,
             'pos': self.pos,
-            'hand': self.hand}
+            'hand': self.hand,
+            'chips': self.chips}
 
 class CardDeck:
   def __init__(self):
-    self.cards = [val+suit for val in '23456789TJQKA' for suit in 'HDSC']
+    self.cards = [val+suit for val in '23456789TJQKA' for suit in 'hdsc']
     random.shuffle(self.cards)
 
   def next_card(self):
@@ -39,15 +46,18 @@ class CardDeck:
 class GameState:
   def __init__(self):
     self.state = None
+    self.pot = None
     self.board = []
-    self.players = [Player('Евгений', 1),
-                    Player('Сергей', 2),
-                    Player('Анна', 3)]
+    CHIPS = 1000
+    self.players = [Player('Евгений', 1, CHIPS),
+                    Player('Сергей', 2, CHIPS),
+                    Player('Анна', 3, CHIPS)]
     self.button_pos = 1
     self.next_phase()
 
   def _start_new(self):
     self.state = PREFLOP
+    self.pot = 0
     for p in self.players:
       p.hand = []
     self.board = []
@@ -80,6 +90,9 @@ class GameState:
         self.button_pos = self.players[0].pos
       self._start_new()
 
+  def make_move(self, pos, action, *args):
+     pass 
+
   def as_dict(self):
     return {'state': self.state,
             'board': self.board,
@@ -88,14 +101,25 @@ class GameState:
 
 game_state = GameState()
 
+def send_full_game_state():
+  emit('full_state', game_state.as_dict(), json=True)
+
+def send_full_game_state():
+  emit('full_state', game_state.as_dict(), json=True, broadcast=True)
+
 @socketio.on('get_full_state')
 def handle_get_full_state():
-  emit('full_state', game_state.as_dict(), json=True)
+  send_full_game_state()
+
+@socketio.on('make_move')
+def handle_make_move(player_pos, action, *args):
+  game_state.make_move(pos, action, *args)
+  send_full_game_state()
 
 @socketio.on('next_phase')
 def handle_next_phase():
   game_state.next_phase()
-  emit('full_state', game_state.as_dict(), json=True, broadcast=True)
+  broadcast_full_game_state()
 
 if __name__ == '__main__':
   socketio.run(app)
